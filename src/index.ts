7fe4b0f296f35b8e0db4b53446b35becfdf02983
Noah Spannbauer <noahspannbauer@icloud.com>
2024-06-17 21:42:02 -0500

    Swapping NextUI for Material Tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import './styles.css';
 
+export * from './components/accordion/Accordion';
+export * from './components/button/Button';
 export * from './components/drawer/Drawer';
-export * from './components/logo/Logo';
+export * from './components/iconButton/IconButton';
+export * from './components/input/Input';
+export * from './components/navbar/Navbar';
 export * from './components/plane/Plane';
 export * from './components/puzzlePiece/PuzzlePiece';
+export * from './components/select/Select';
+export * from './components/typography/Typography';
