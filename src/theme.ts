92ad49c872a43f4784d1bee870a1c8c1843b8a2e
Noah Spannbauer <noahspannbauer@icloud.com>
2024-07-04 19:39:28 -0500

    adding date picker component

---
+++ src/theme.ts
@@ -0,0 +1,36 @@
+export const theme = {
+  dialog: {
+    styles: {
+      xs: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      },
+      sm: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      },
+      md: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      },
+      lg: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      },
+      xl: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      },
+      xxl: {
+        width: '',
+        minWidth: '',
+        maxWidth: ''
+      }
+    }
+  }
+};
