40970148dcf2042620408ad9aa6afbf6ea456ef1
Noah Spannbauer <noahspannbauer@icloud.com>
2024-07-05 14:29:08 -0500

    adding drawer sub components

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -73,5 +73,5 @@ type DrawerBaseProps = Pick<
 export interface DrawerProps extends DrawerBaseProps {}
 
 export const Drawer = ({ ...rest }: DrawerBaseProps) => {
-  return <MaterialTailwindDrawer className='flex p-10' {...rest} />;
+  return <MaterialTailwindDrawer className='p-10' {...rest} />;
 };
