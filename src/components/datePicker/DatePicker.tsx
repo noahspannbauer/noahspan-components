53e9312a8d2dd93cbd44f5bff5fbf61fa0b27203
Noah Spannbauer <noahspannbauer@icloud.com>
2024-07-04 19:52:44 -0500

    adding date picker component

--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,10 +1,4 @@
 import { useState } from 'react';
-// import {
-//   Input,
-//   Popover,
-//   PopoverHandler,
-//   PopoverContent
-// } from '@material-tailwind/react';
 import { CalendarIcon } from '../icons/calendar/CalendarIcon';
 import { ChevronLeft } from '../icons/chevronLeft/ChevronLeft';
 import { ChevronRight } from '../icons/chevronRight/ChevronRight';
@@ -13,8 +7,6 @@ import { IconButton } from '../iconButton/IconButton';
 import { Input } from '../input/Input';
 import { format } from 'date-fns';
 import { DayPicker } from 'react-day-picker';
-import { ThemeProvider } from '@material-tailwind/react';
-import { theme } from '../../theme';
 
 export const DatePicker = () => {
   const [date, setDate] = useState<Date>();
@@ -30,107 +22,66 @@ export const DatePicker = () => {
   };
 
   return (
-    <ThemeProvider value={theme}>
-      <div className='relative flex w-full max-w-[24rem]'>
-        <Input
-          label='Date'
-          className='pr-20'
-          containerProps={{
-            className: 'min-w-0'
-          }}
-          value={date ? format(date, 'MM/dd/yyyy') : ''}
-        />
-        <IconButton
-          size='sm'
-          className='!absolute right-1 top-1 rounded'
-          onClick={handleIsDialogOpen}
-          variant='text'
-        >
-          <CalendarIcon />
-        </IconButton>
-        <Dialog
-          open={isDialogOpen}
-          handler={handleIsDialogOpen}
-          className='!w-fit'
-          size='xs'
-        >
-          <DialogBody>
-            <DayPicker
-              mode='single'
-              selected={date}
-              onSelect={handleDateSelected}
-              showOutsideDays
-              className='border-0'
-              classNames={{
-                caption: 'flex justify-center py-2 mb-4 relative items-center',
-                caption_label: 'text-sm font-medium text-gray-900',
-                nav: 'flex items-center',
-                nav_button:
-                  'h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300',
-                nav_button_previous: 'absolute left-1.5',
-                nav_button_next: 'absolute right-1.5',
-                table: 'border-collapse',
-                head_row: 'flex font-medium text-gray-900',
-                head_cell: 'm-0.5 w-9 font-normal text-sm',
-                row: 'flex w-full mt-2',
-                cell: 'text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-                day: 'h-9 w-9 p-0 font-normal',
-                day_range_end: 'day-range-end',
-                day_selected:
-                  'rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white',
-                day_today: 'rounded-md bg-gray-200 text-gray-900',
-                day_outside:
-                  'day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10',
-                day_disabled: 'text-gray-500 opacity-50',
-                day_hidden: 'invisible'
-              }}
-              components={{
-                IconLeft: () => <ChevronLeft />,
-                IconRight: () => <ChevronRight />
-              }}
-            />
-          </DialogBody>
-        </Dialog>
-      </div>
-    </ThemeProvider>
+    <div className='relative flex w-full max-w-[24rem]'>
+      <Input
+        label='Date'
+        className='pr-20'
+        containerProps={{
+          className: 'min-w-0'
+        }}
+        value={date ? format(date, 'MM/dd/yyyy') : ''}
+      />
+      <IconButton
+        size='sm'
+        className='!absolute right-1 top-1 rounded'
+        onClick={handleIsDialogOpen}
+        variant='text'
+      >
+        <CalendarIcon />
+      </IconButton>
+      <Dialog
+        open={isDialogOpen}
+        handler={handleIsDialogOpen}
+        className='!w-fit'
+        size='xs'
+      >
+        <DialogBody>
+          <DayPicker
+            mode='single'
+            selected={date}
+            onSelect={handleDateSelected}
+            showOutsideDays
+            className='border-0'
+            classNames={{
+              caption: 'flex justify-center py-2 mb-4 relative items-center',
+              caption_label: 'text-sm font-medium text-gray-900',
+              nav: 'flex items-center',
+              nav_button:
+                'h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300',
+              nav_button_previous: 'absolute left-1.5',
+              nav_button_next: 'absolute right-1.5',
+              table: 'border-collapse',
+              head_row: 'flex font-medium text-gray-900',
+              head_cell: 'm-0.5 w-9 font-normal text-sm',
+              row: 'flex w-full mt-2',
+              cell: 'text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
+              day: 'h-9 w-9 p-0 font-normal',
+              day_range_end: 'day-range-end',
+              day_selected:
+                'rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white',
+              day_today: 'rounded-md bg-gray-200 text-gray-900',
+              day_outside:
+                'day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10',
+              day_disabled: 'text-gray-500 opacity-50',
+              day_hidden: 'invisible'
+            }}
+            components={{
+              IconLeft: () => <ChevronLeft />,
+              IconRight: () => <ChevronRight />
+            }}
+          />
+        </DialogBody>
+      </Dialog>
+    </div>
   );
-
-  // return (
-  //   <div className='p-24'>
-  //         <DayPicker
-  //           mode='single'
-  //           selected={date}
-  //           onSelect={setDate}
-  //           showOutsideDays
-  //           className='border-0'
-  //           classNames={{
-  //             caption: 'flex justify-center py-2 mb-4 relative items-center',
-  //             caption_label: 'text-sm font-medium text-gray-900',
-  //             nav: 'flex items-center',
-  //             nav_button:
-  //               'h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300',
-  //             nav_button_previous: 'absolute left-1.5',
-  //             nav_button_next: 'absolute right-1.5',
-  //             table: 'w-full border-collapse',
-  //             head_row: 'flex font-medium text-gray-900',
-  //             head_cell: 'm-0.5 w-9 font-normal text-sm',
-  //             row: 'flex w-full mt-2',
-  //             cell: 'text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
-  //             day: 'h-9 w-9 p-0 font-normal',
-  //             day_range_end: 'day-range-end',
-  //             day_selected:
-  //               'rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white',
-  //             day_today: 'rounded-md bg-gray-200 text-gray-900',
-  //             day_outside:
-  //               'day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10',
-  //             day_disabled: 'text-gray-500 opacity-50',
-  //             day_hidden: 'invisible'
-  //           }}
-  //           components={{
-  //             IconLeft: () => <ChevronLeft />,
-  //             IconRight: () => <ChevronRight />
-  //           }}
-  //         />
-  //   </div>
-  // );
 };
