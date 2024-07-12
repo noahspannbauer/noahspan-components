c8212f1799f5297ebb13dd1ba20e0c3db76e5cda
noahspannbauer <noahspannbauer@icloud.com>
2024-07-12 07:42:55 -0500

    Feature/26 add list component (#27)
    * adding list component
    
    * adding list component

---
+++ src/components/list/List.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { List, ListItem, ListItemPrefix } from './List';
+import { PlaneIcon } from '../icons/plane/PlaneIcon';
+import { PuzzlePieceIcon } from '../icons/puzzlePiece/PuzzlePieceIcon';
+import { Typography } from '../typography/Typography';
+
+const meta: Meta<typeof List> = {
+  title: 'Components/List',
+  component: List
+} satisfies Meta<typeof List>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  render: function Render() {
+    return (
+      <List>
+        <ListItem>One</ListItem>
+        <ListItem>Two</ListItem>
+        <ListItem>Three</ListItem>
+      </List>
+    );
+  }
+};
+
+export const Prefix: Story = {
+  render: function Render() {
+    return (
+      <List>
+        <ListItem>
+          <ListItemPrefix>
+            <PlaneIcon />
+          </ListItemPrefix>
+          <div>
+            <Typography variant='h6'>Flying</Typography>
+            <Typography variant='small'>flying.noahspannbauer.com</Typography>
+          </div>
+        </ListItem>
+        <ListItem>
+          <ListItemPrefix>
+            <PuzzlePieceIcon />
+          </ListItemPrefix>
+          <div>
+            <Typography variant='h6'>Components</Typography>
+            <Typography variant='small'>
+              components.noahspannbauer.com
+            </Typography>
+          </div>
+        </ListItem>
+      </List>
+    );
+  }
+};
