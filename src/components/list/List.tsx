ab7be54393413e8ed4ed7f1dc997a0403225d3b7
Noah Spannbauer <noahspannbauer@icloud.com>
2024-07-12 07:35:07 -0500

    adding list component

---
+++ src/components/list/List.tsx
@@ -0,0 +1,51 @@
+import {
+  List as MaterialTailwindList,
+  ListProps as MaterialTailwindListProps,
+  ListItem as MaterialTailwindListItem,
+  ListItemProps as MaterialTailwindListItemProps,
+  ListItemPrefix as MaterialTailwindListItemPrefix,
+  ListItemPrefixProps as MaterialTailwindListItemPrefixProps,
+  ListItemSuffix as MaterialTailwindListItemSuffix,
+  ListItemSuffixProps as MaterialTailwindListItemSuffixProps
+} from '@material-tailwind/react';
+
+type ListItemBaseProps = Pick<
+  MaterialTailwindListItemProps,
+  'ripple' | 'selected' | 'disabled' | 'className' | 'children'
+>;
+
+export interface ListItemProps extends ListItemBaseProps {}
+
+export const ListItem = ({ ...rest }: ListItemProps) => {
+  return <MaterialTailwindListItem {...rest} />;
+};
+
+type ListItemPrefixBaseProps = Pick<
+  MaterialTailwindListItemPrefixProps,
+  'className' | 'children'
+>;
+
+export interface ListItemPrefixProps extends ListItemPrefixBaseProps {}
+
+export const ListItemPrefix = ({ ...rest }: ListItemPrefixProps) => {
+  return <MaterialTailwindListItemPrefix {...rest} />;
+};
+
+type ListItemSuffixBaseProps = Pick<
+  MaterialTailwindListItemSuffixProps,
+  'className' | 'children'
+>;
+
+export interface ListItemSuffixProps extends ListItemSuffixBaseProps {}
+
+export const ListItemSuffix = ({ ...rest }: ListItemSuffixProps) => {
+  return <MaterialTailwindListItemSuffix {...rest} />;
+};
+
+type ListBaseProps = Pick<MaterialTailwindListProps, 'className' | 'children'>;
+
+export interface ListProps extends ListBaseProps {}
+
+export const List = ({ ...rest }: ListProps) => {
+  return <MaterialTailwindList {...rest} />;
+};
