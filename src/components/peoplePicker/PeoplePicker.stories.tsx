46fc3f89436185a6c0125a164ce1de8f669abf6e
noahspannbauer <noahspannbauer@icloud.com>
2024-07-13 07:27:48 -0500

    Feature/24 add people picker (#29)
    * adding people picker
    
    * adding people picker
    
    * adding people picker
    
    * adding people picker
    
    * adding people picker

---
+++ src/components/peoplePicker/PeoplePicker.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useArgs } from '@storybook/preview-api';
+import { PeoplePicker, Person } from './PeoplePicker';
+
+const meta: Meta<typeof PeoplePicker> = {
+  title: 'Components/PeoplePicker',
+  component: PeoplePicker
+} satisfies Meta<typeof PeoplePicker>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Outlined: Story = {
+  args: {
+    results: [],
+    loading: false
+  },
+  render: function Render(args) {
+    const [{ _results }, updateArgs] = useArgs();
+    const people: Person[] = [
+      {
+        displayName: 'Adam'
+      },
+      {
+        displayName: 'Catherine'
+      },
+      {
+        displayName: 'Nathan'
+      },
+      {
+        displayName: 'Noah'
+      },
+      {
+        displayName: 'Zoey'
+      }
+    ];
+
+    const handleOnChange = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+      const searchString: string = event.target.value;
+      let results: Person[];
+
+      if (event.target.value !== '') {
+        results = people.filter((person: Person) => {
+          if (person.displayName.startsWith(searchString)) {
+            return person;
+          }
+        });
+      } else {
+        results = [];
+      }
+
+      updateArgs({ results: results });
+    };
+
+    return (
+      <PeoplePicker
+        inputProps={{
+          label: 'Name',
+          onChange: handleOnChange
+        }}
+        {...args}
+      />
+    );
+  }
+};
---
+++ src/components/peoplePicker/PeoplePicker.tsx
@@ -0,0 +1,50 @@
+import { Card } from '../card/Card';
+import { Input, InputProps } from '../input/Input';
+import { List, ListProps, ListItem, ListItemProps } from '../list/List';
+import { Spinner } from '../spinner/Spinner';
+
+export type Person = {
+  displayName: string;
+};
+
+export interface PeoplePickerProps {
+  results: Person[];
+  inputProps?: InputProps;
+  listProps?: ListProps;
+  listItemProps?: ListItemProps;
+  loading: boolean;
+}
+
+export const PeoplePicker = ({
+  results,
+  inputProps,
+  listProps,
+  listItemProps,
+  loading,
+  ...rest
+}: PeoplePickerProps) => {
+  return (
+    <div {...rest}>
+      <Input {...inputProps} />
+      {results.length > 0 && (
+        <Card>
+          <List {...listProps}>
+            {!loading &&
+              results.map((person: Person) => {
+                return (
+                  <ListItem {...listItemProps}>{person.displayName}</ListItem>
+                );
+              })}
+            {loading && (
+              <ListItem {...listItemProps}>
+                <div className='grid justify-items-center'>
+                  <Spinner size='sm' />
+                </div>
+              </ListItem>
+            )}
+          </List>
+        </Card>
+      )}
+    </div>
+  );
+};
