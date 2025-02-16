02af86f4b637e6b69899856fe13d99a9be155f9c
Noah Spannbauer <noahspannbauer@icloud.com>
2025-02-16 08:33:21 -0600

    changing iconName prop to enum

--- src/components/icon/Icon.stories.tsx
+++ src/components/icon/Icon.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Icon } from './Icon';
+import { Icon, IconName } from './Icon';
 
 const meta: Meta<typeof Icon> = {
   title: 'Components/Icons',
@@ -13,244 +13,265 @@ type Story = StoryObj<typeof meta>;
 export const Angular: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-angular'
+    iconName: IconName.ANGULAR
   }
 };
 
 export const ArrowUpRightFromSquare: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-arrow-up-right-from-square'
+    iconName: IconName.ARROW_UP_RIGHT_FROM_SQUARE
   }
 };
 
 export const Baseball: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-baseball'
+    iconName: IconName.BASEBALL
   }
 };
 
 export const Calendar: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-calendar'
+    iconName: IconName.CALENDAR
+  }
+};
+
+export const Camera: Story = {
+  args: {
+    size: '2xl',
+    iconName: IconName.CAMERA
   }
 };
 
 export const ChevronDown: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-chevron-down'
+    iconName: IconName.CHEVRON_DOWN
   }
 };
 
 export const ChevronLeft: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-chevron-left'
+    iconName: IconName.CHEVRON_LEFT
   }
 };
 
 export const ChevronRight: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-chevron-right'
+    iconName: IconName.CHEVRON_RIGHT
   }
 };
 
 export const ChevronUp: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-chevron-up'
+    iconName: IconName.CHEVRON_UP
   }
 };
 
 export const CircleCheck: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-circle-check'
+    iconName: IconName.CIRCLE_CHECK
   }
 };
 
 export const CircleInfo: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-circle-info'
+    iconName: IconName.CIRCLE_INFO
   }
 };
 
 export const CircleXMark: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-circle-xmark'
+    iconName: IconName.CIRCLE_XMARK
   }
 };
 
 export const Edit: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-edit'
+    iconName: IconName.EDIT
   }
 };
 
 export const EllipsisVertical: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-ellipsis-vertical'
+    iconName: IconName.ELLIPSIS_VERTICAL
   }
 };
 
 export const Eye: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-eye'
+    iconName: IconName.EYE
   }
 };
 
 export const Gear: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-gear'
+    iconName: IconName.GEAR
   }
 };
 
 export const GitHub: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-github'
+    iconName: IconName.GITHUB
   }
 };
 
 export const Globe: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-globe'
+    iconName: IconName.GLOBE
   }
 };
 
 export const GoLang: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-golang'
+    iconName: IconName.GOLANG
   }
 };
 
 export const Heart: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-heart'
+    iconName: IconName.HEART
   }
 };
 
 export const Java: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-java'
+    iconName: IconName.JAVA
   }
 };
 
 export const JavaScript: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-js'
+    iconName: IconName.JAVASCRIPT
   }
 };
 
 export const LinkedIn: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-linkedin'
+    iconName: IconName.LINKEDIN
+  }
+};
+
+export const MapLocationDot: Story = {
+  args: {
+    size: '2xl',
+    iconName: IconName.MAP_LOCATION_DOT
   }
 };
 
 export const Pen: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-pen'
+    iconName: IconName.PEN
   }
 };
 
 export const Plane: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-plane'
+    iconName: IconName.PLANE
   }
 };
 
 export const Plus: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-plus'
+    iconName: IconName.PLUS
   }
 };
 
 export const PuzzlePiece: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-puzzle-piece'
+    iconName: IconName.PUZZLE_PIECE
   }
 };
 
 export const Python: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-python'
+    iconName: IconName.PYTHON
   }
 };
 
 export const Save: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-save'
+    iconName: IconName.SAVE
   }
 };
 
 export const SignOut: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-sign-out'
+    iconName: IconName.SIGN_OUT
+  }
+};
+
+export const Ticket: Story = {
+  args: {
+    size: '2xl',
+    iconName: IconName.TICKET
   }
 };
 
 export const Trash: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-trash'
+    iconName: IconName.TRASH
   }
 };
 
 export const TriangleExclamation: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-triangle-exclamation'
+    iconName: IconName.TRANGLE_EXCLAMATION
   }
 };
 
 export const Truck: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-truck-fast'
+    iconName: IconName.TRUCK_FAST
   }
 };
 
 export const UserTie: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-user-tie'
+    iconName: IconName.USER_TIE
   }
 };
 
 export const Vue: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-vuejs'
+    iconName: IconName.VUE
   }
 };
 
 export const XMark: Story = {
   args: {
     size: '2xl',
-    iconName: 'fa-xmark'
+    iconName: IconName.XMARK
   }
 };
