50e72682e6217df00868cf8cde5296027f966420
Noah Spannbauer <noahspannbauer@icloud.com>
2025-04-19 11:21:33 -0500

    adding stack component

---
+++ src/components/stack/Stack.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Stack } from './Stack';
+import { Card, CardContent } from '../card/Card';
+import { Typography } from '../typography/Typography';
+
+const meta: Meta<typeof Stack> = {
+  title: 'Components/Stack',
+  component: Stack
+} satisfies Meta<typeof Stack>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <>
+        <Stack spacing={5}>
+          {[...Array(3)].map((_element, index) => {
+            return (
+              <Card>
+                <CardContent>
+                  <Typography variant='body1'>{`Item ${index}`}</Typography>
+                </CardContent>
+              </Card>
+            );
+          })}
+        </Stack>
+      </>
+    );
+  }
+};
