21968cff402ea239446742f82712eb0098f6e189
Noah Spannbauer <noahspannbauer@icloud.com>
2025-07-02 14:15:03 -0500

    switching button to heroui

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,24 +1,3 @@
-// import {
-//   Button as MuiButton,
-//   ButtonProps as MuiButtonProps
-// } from '@mui/material';
-// import { ThemeProvider } from '@mui/material/styles';
-// import theme from '../../theme';
-
-// type ButtonBaseProps = MuiButtonProps;
-
-// export interface ButtonProps extends ButtonBaseProps {
-//   loading?: boolean;
-// }
-
-// export const Button = ({ loading, ...rest }: ButtonProps) => {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <MuiButton {...rest} />
-//     </ThemeProvider>
-//   );
-// };
-
 import {
   Button as HeroUIButton,
   ButtonProps as HeroUIButtonProps,
