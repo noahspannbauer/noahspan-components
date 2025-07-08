bf8c219913ac709af47d940c21b0684dba206efa
Noah Spannbauer <noahspannbauer@icloud.com>
2025-07-08 12:48:56 -0500

    switching skeleton to heroui

--- src/components/skeleton/Skeleton.tsx
+++ src/components/skeleton/Skeleton.tsx
@@ -1,12 +1,30 @@
-import {
-  Skeleton as MuiSkeleton,
-  SkeletonProps as MuiSkeletonProps
-} from '@mui/material';
+// import {
+//   Skeleton as MuiSkeleton,
+//   SkeletonProps as MuiSkeletonProps
+// } from '@mui/material';
 
-type SkeletonBaseProps = MuiSkeletonProps;
+// type SkeletonBaseProps = MuiSkeletonProps;
+
+// export interface SkeletonProps extends SkeletonBaseProps {}
+
+// export const Skeleton = ({ ...rest }: SkeletonProps) => {
+//   return <MuiSkeleton {...rest} />;
+// };
+
+import {
+  Skeleton as HeroUISkeleton,
+  SkeletonProps as HeroUISkeletonProps,
+  HeroUIProvider
+} from '@heroui/react';
+
+type SkeletonBaseProps = HeroUISkeletonProps;
 
 export interface SkeletonProps extends SkeletonBaseProps {}
 
 export const Skeleton = ({ ...rest }: SkeletonProps) => {
-  return <MuiSkeleton {...rest} />;
+  return (
+    <HeroUIProvider>
+      <HeroUISkeleton {...rest} />
+    </HeroUIProvider>
+  );
 };
