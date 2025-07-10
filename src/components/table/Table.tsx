9d93093d5e36c45afa1d9d5a1017e3f820c96837
Noah Spannbauer <noahspannbauer@icloud.com>
2025-07-10 08:16:31 -0500

    switching table to heroui

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,19 +1,34 @@
+// import {
+//   Table as MuiTable,
+//   TableProps as MuiTableProps,
+//   TableBody as MuiTableBody,
+//   TableBodyProps as MuiTableBodyProps,
+//   TableCell as MuiTableCell,
+//   TableCellProps as MuiTableCellProps,
+//   TableContainer as MuiTableContainer,
+//   TableContainerProps as MuiTableContainerProps,
+//   TableHead as MuiTableHead,
+//   TableHeadProps as MuiTableHeadProps,
+//   TablePagination as MuiTablePagination,
+//   TablePaginationProps as MuiTablePaginationProps,
+//   TableRow as MuiTableRow,
+//   TableRowProps as MuiTableRowProps
+// } from '@mui/material';
 import {
-  Table as MuiTable,
-  TableProps as MuiTableProps,
-  TableBody as MuiTableBody,
-  TableBodyProps as MuiTableBodyProps,
-  TableCell as MuiTableCell,
-  TableCellProps as MuiTableCellProps,
-  TableContainer as MuiTableContainer,
-  TableContainerProps as MuiTableContainerProps,
-  TableHead as MuiTableHead,
-  TableHeadProps as MuiTableHeadProps,
-  TablePagination as MuiTablePagination,
-  TablePaginationProps as MuiTablePaginationProps,
-  TableRow as MuiTableRow,
-  TableRowProps as MuiTableRowProps
-} from '@mui/material';
+  Table as HeroUITable,
+  TableProps as HeroUITableProps,
+  TableBody as HeroUITableBody,
+  TableColumn as HeroUITableColumn,
+  TableColumnProps as HeroUITableColumnProps,
+  TableBodyProps as HeroUITableBodyProps,
+  TableCell as HeroUITableCell,
+  TableCellProps as HeroUITableCellProps,
+  TableHeader as HeroUITableHeader,
+  TableHeaderProps as HeroUITableHeaderProps,
+  TableRow as HeroUITableRow,
+  TableRowProps as HeroUITableRowProps,
+  HeroUIProvider
+} from '@heroui/react'
 import { Paper } from '../paper/Paper';
 import { TablePaginationActions } from './TablePaginationActions';
 import {
@@ -25,93 +40,127 @@ import {
   HeaderContext,
   useReactTable
 } from '@tanstack/react-table';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../../theme';
 
 export type { CellContext };
 export type { ColumnDef };
 export type { HeaderContext };
 
-type TableBodyBaseProps = MuiTableBodyProps;
+type TableBodyBaseProps = Pick<HeroUITableBodyProps<unknown>, 
+  | 'children'
+  | 'items'
+  | 'isLoading'
+  | 'loadingContent'
+  | 'emptyContent'
+>;
 
 export interface TableBodyProps extends TableBodyBaseProps {}
 
 export const TableBody = ({ ...rest }: TableBodyProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiTableBody {...rest} />
-    </ThemeProvider>
+    <HeroUIProvider>
+      <HeroUITableBody {...rest} />
+    </HeroUIProvider>
   );
 };
 
-type TableCellBaseProps = MuiTableCellProps;
+type TableCellBaseProps = HeroUITableCellProps;
 
 export interface TableCellProps extends TableCellBaseProps {}
 
 export const TableCell = ({ ...rest }: TableCellProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiTableCell {...rest} />
-    </ThemeProvider>
+    <HeroUIProvider>
+      <HeroUITableCell {...rest} />
+    </HeroUIProvider> 
   );
 };
 
-type TableContainerBaseProps = MuiTableContainerProps;
+type TableColumnBaseProps = Pick<
+  HeroUITableColumnProps<unknown>,
+  | 'children'
+  | 'align'
+  | 'hideHeader'
+  | 'allowsSorting'
+  | 'isRowHeader'
+  | 'textValue'
+  | 'width'
+  | 'minWidth'
+  | 'maxWidth'
+>;
 
-export interface TableContainerProps extends TableContainerBaseProps {}
+export interface TableColumnProps extends TableColumnBaseProps {};
 
-export const TableContainer = ({ ...rest }: TableContainerProps) => {
+export const TableColumn = ({ ...rest }: TableColumnProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiTableContainer {...rest} />
-    </ThemeProvider>
-  );
-};
+    <HeroUIProvider>
+      <HeroUITableColumn {...rest} />
+    </HeroUIProvider>
+  )
+}
 
-type TableHeadBaseProps = MuiTableHeadProps;
+// type TableContainerBaseProps = MuiTableContainerProps;
 
-export interface TableHeadProps extends TableHeadBaseProps {}
+// export interface TableContainerProps extends TableContainerBaseProps {}
 
-export const TableHead = ({ ...rest }: TableHeadProps) => {
+// export const TableContainer = ({ ...rest }: TableContainerProps) => {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <MuiTableContainer {...rest} />
+//     </ThemeProvider>
+//   );
+// };
+
+type TableHeaderBaseProps = Pick<
+  HeroUITableHeaderProps<unknown>,
+  | 'children'
+  | 'columns'  
+>;
+
+export interface TableHeaderProps extends TableHeaderBaseProps {}
+
+export const TableHeader = ({ ...rest }: TableHeaderProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiTableHead {...rest} />
-    </ThemeProvider>
+    <HeroUIProvider>
+      <HeroUITableHeader {...rest} />
+    </HeroUIProvider>
   );
 };
 
-type TablePaginationBaseProps = MuiTablePaginationProps;
+// type TablePaginationBaseProps = MuiTablePaginationProps;
 
-export type TablePaginationProps = TablePaginationBaseProps;
+// export type TablePaginationProps = TablePaginationBaseProps;
 
-export const TablePagination = ({ ...rest }: TablePaginationProps) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <MuiTablePagination {...rest} />
-    </ThemeProvider>
-  );
-};
+// export const TablePagination = ({ ...rest }: TablePaginationProps) => {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <MuiTablePagination {...rest} />
+//     </ThemeProvider>
+//   );
+// };
 
-type TableRowBaseProps = MuiTableRowProps;
+type TableRowBaseProps = HeroUITableRowProps;
 
 export interface TableRowProps extends TableRowBaseProps {}
 
 export const TableRow = ({ ...rest }: TableRowProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <MuiTableRow {...rest} />
-    </ThemeProvider>
+    <HeroUIProvider>
+      <HeroUITableRow {...rest} />
+    </HeroUIProvider>
   );
 };
 
-type TableBaseProps = MuiTableProps;
+// type TableBaseProps = Omit<
+//   HeroUITableProps,
+//   | 'children'
+// >;
 
-export interface TableProps extends TableBaseProps {
+export interface TableProps {
   columns: any;
   data: any;
 }
 
-export const Table = ({ columns, data, ...rest }: TableProps) => {
+export const Table = ({ columns, data, ...rest }) => {
   const table = useReactTable({
     data,
     columns,
@@ -122,89 +171,91 @@ export const Table = ({ columns, data, ...rest }: TableProps) => {
   const { pageSize, pageIndex } = table.getState().pagination;
 
   return (
-    <ThemeProvider theme={theme}>
-      <TableContainer component={Paper}>
-        <MuiTable {...rest}>
-          <TableHead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableCell
-                      key={header.id}
-                      colSpan={header.colSpan}
-                      align={header.column.columnDef.meta?.headerAlign}
-                    >
-                      {header.isPlaceholder ? null : (
-                        <div>
-                          {flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                          {/* {header.column.getCanFilter() ? (
-                            <div>
-                              <Filter column={header.column} table={table} />
-                            </div>
-                          ) : null} */}
-                        </div>
-                      )}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHead>
-          <TableBody>
+    <HeroUIProvider>
+        <HeroUITable>
+          <HeroUITableHeader>
+            <HeroUITableColumn>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <HeroUITableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <HeroUITableCell
+                        key={header.id}
+                        // colSpan={header.colSpan}
+                        // align={header.column.columnDef.meta?.headerAlign}
+                      >
+                        {header.isPlaceholder ? null : (
+                          <div>
+                            {flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                            {/* {header.column.getCanFilter() ? (
+                              <div>
+                                <Filter column={header.column} table={table} />
+                              </div>
+                            ) : null} */}
+                          </div>
+                        )}
+                      </HeroUITableCell>
+                    );
+                  })}
+                </HeroUITableRow>
+              ))}
+            </HeroUITableColumn>
+          </HeroUITableHeader>
+          <HeroUITableBody>
             <>
               {table.getRowModel().rows.map((row) => {
                 return (
-                  <TableRow key={row.id}>
+                  <HeroUITableRow key={row.id}>
                     {row.getVisibleCells().map((cell) => {
                       return (
-                        <TableCell
+                        <HeroUITableCell
                           key={cell.id}
-                          align={cell.column.columnDef.meta?.align}
+                          // align={cell.column.columnDef.meta?.align}
                         >
                           {flexRender(
                             cell.column.columnDef.cell,
                             cell.getContext()
                           )}
-                        </TableCell>
+                        </HeroUITableCell>
                       );
                     })}
-                  </TableRow>
+                  </HeroUITableRow>
                 );
               })}
             </>
-          </TableBody>
-          <TableHead>
-            {table.getFooterGroups().map((footerGroup, index) => {
-              if (index === 0) {
-                return (
-                  <TableRow key={footerGroup.id}>
-                    {footerGroup.headers.map((header) => {
-                      return (
-                        <TableCell
-                          key={header.id}
-                          align={header.column.columnDef.meta?.align}
-                        >
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.footer,
-                                header.getContext()
-                              )}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              }
-            })}
-          </TableHead>
-        </MuiTable>
-      </TableContainer>
-      <TablePagination
+          </HeroUITableBody>
+          <HeroUITableHeader>
+            <HeroUITableColumn>
+              {table.getFooterGroups().map((footerGroup, index) => {
+                if (index === 0) {
+                  return (
+                    <HeroUITableRow key={footerGroup.id}>
+                      {footerGroup.headers.map((header) => {
+                        return (
+                          <HeroUITableCell
+                            key={header.id}
+                            // align={header.column.columnDef.meta?.align}
+                          >
+                            {header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                  header.column.columnDef.footer,
+                                  header.getContext()
+                                )}
+                          </HeroUITableCell>
+                        );
+                      })}
+                    </HeroUITableRow>
+                  );
+                }
+              })}
+            </HeroUITableColumn>
+          </HeroUITableHeader>
+        </HeroUITable>
+      {/* <TablePagination
         rowsPerPageOptions={[10, 25, 50, { label: 'All', value: data.length }]}
         component='div'
         count={table.getFilteredRowModel().rows.length}
@@ -225,7 +276,7 @@ export const Table = ({ columns, data, ...rest }: TableProps) => {
           table.setPageSize(size);
         }}
         ActionsComponent={TablePaginationActions}
-      />
-    </ThemeProvider>
+      /> */}
+    </HeroUIProvider>
   );
 };
